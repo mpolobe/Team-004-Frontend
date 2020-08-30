abcccc9f75061b66e2e4c290e1d5f693c71cc438
uzor uchenna <ucheuzor24@gmail.com>
2020-08-30 11:27:57 +0100

    basic cleanup

--- afriteach/src/App.js
+++ afriteach/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 
 function App() {
   return (
-    <div className='containter'>
+    <div className="containter">
       <div className="jumbotron text-center">
         <h1>AfriTeach website</h1>
         <p className="lead">Welcome to Afriteach, A platform that helps make learning easy in Africa.</p>
